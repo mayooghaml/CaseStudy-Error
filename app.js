1fae1592e7de442d83eb451e51fb83036a9b6bfe
mayooghaml <mayooghamuraleedharan@gmail.com>
2022-01-01 10:38:59 +0530

    new

--- app.js
+++ app.js
@@ -60,6 +60,6 @@ app.get('/',function(req,res){
 
 
 
-app.listen(3000, ()=>{//part#1 point5
-    console.log("Server Ready on 3000"); 
+app.listen(5000, ()=>{//part#1 point5
+    console.log("Server Ready on 5000"); 
 });
